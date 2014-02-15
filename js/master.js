7dc8589bd353475842dba41141c337e8c82beee1
Philip Del Vecchio <philipjdelvecchio@gmail.com>
2014-02-15 00:14:12 -0500

    going home probably

--- js/master.js
+++ js/master.js
@@ -3,9 +3,19 @@ var kickBuffer;
 var pads;
 
 $(document).ready(function() {
-  var editMode = false; // false = performance
+   
+  // AudioContext
+  try {
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    context = new AudioContext();
+  } catch(e) {
+    alert('Web Audio API is not supported in this browser');
+  }
+
   pads = new Array(64);
   
+  // Mode buttons
+  var editMode = false; // false = performance
   $('#performance').click(function() { 
     editMode = false;
     $('#performance').color = "red";
@@ -17,6 +27,7 @@ $(document).ready(function() {
     $('#edit').color = "red";
   });
 
+  //Initialize all the pads
   for (var i = 0; i < 64; i++) {
     
     // Set the pad's onclick
@@ -29,15 +40,23 @@ $(document).ready(function() {
       }
     })
 
-    $('#' + i).html('<span class="pad-label">' + i + '</span>';
+    // Names inside pads
+    $('#' + i).html('<span class="pad-label">' + i + '</span>');
+    var gainNode = context.createGain();
+    gainNode.gain.value = 1.0;
 
     pads[i] = {
       'name': i.toString(),
+      'url': null,
+      'buffer': null,
+      'gainNode': gainNode,
+    };
 
-    }
+    loadSound(i);
   }
 
-  i = 69;
+  pads[0].url = "http://soundjax.com/reddo/36915%5Ekick.mp3";
+  loadSound(0);
 
 });
 
@@ -69,15 +88,16 @@ function init() {
 }
 **/
 
-function loadSound(url) {
+function loadSound(i) {
+  if (!pads[i].url) return;
   var request = new XMLHttpRequest();
-  request.open('GET', url, true);
+  request.open('GET', pads[i].url, true);
   request.responseType = 'arraybuffer';
 
   request.onload = function() {
     context.decodeAudioData(
       request.response, 
-      function(buffer) { kickBuffer = buffer; },
+      function(buffer) { pads[i].buffer = buffer; },
       onError
     )
   }
@@ -89,11 +109,12 @@ function onError() {
   alert('Something went wrong');
 }
 
-function playSound(buffer) {
-  if (!buffer) return;
+function playSound(i) {
+  if (!pads[i].buffer) return;
   console.log("yep");
   var source = context.createBufferSource();
-  source.buffer = buffer;
+  source.buffer = pads[i].buffer;
+  source.connect(pads[i].gainNode);
   source.connect(context.destination);
   source.start(0);
 }
